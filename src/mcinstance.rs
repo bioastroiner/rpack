d9a1de3657b2e65daf4c26dc4cf0abe4b038ec18
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2024-03-21 18:45:38 +0330

    + metadata tests

--- src/mcinstance.rs
+++ src/mcinstance.rs
@@ -1,6 +1,7 @@
 // Module responsible for handling mcmodinstance files
 
 use serde::{Deserialize, Serialize};
+use toml::toml;
 
 /// metadata.packconfig
 #[derive(Serialize, Deserialize)]
@@ -20,6 +21,8 @@ pub struct MeteaData {
     description = [A short description, only on one line]
     version = [Your pack version]
     */
+    #[serde(rename = "formatVersion")]
+    format_version: u8,
     #[serde(rename = "modLoader")]
     mod_loader: ModLoader,
     pack: Pack,
@@ -28,7 +31,7 @@ pub struct MeteaData {
 struct ModLoader {
     #[serde(rename = "type")]
     type_cfg: String,
-    version: u32,
+    version: String,
     #[serde(rename = "minecraftVersion")]
     minecraft_version: String,
 }
@@ -46,8 +49,8 @@ struct Optionals {}
 /// houses download links to jars
 struct Resources {}
 
-pub fn read_metadata(metadata_toml:&String) -> MeteaData {
-    let metadata : MeteaData = toml::from_str(metadata_toml).unwrap();
+pub fn read_metadata(metadata_toml: &str) -> MeteaData {
+    let metadata: MeteaData = toml::from_str(metadata_toml).unwrap();
     metadata
 }
 pub fn read_options() -> Optionals {
@@ -55,4 +58,42 @@ pub fn read_options() -> Optionals {
 }
 pub fn read_resources() -> Resources {
     todo!()
+}
+
+#[test]
+fn test_mcinstance() {
+    let meta_toml = r#"
+    [file]
+    formatVersion = 1 
+
+    [modloader]
+    type = "forge"
+    version = 1614
+    minecraftVersion = "1.7.10"
+
+    [pack]
+    name = "pack name"
+    author = "Your name"
+    description = "A short description, only on one line"
+    version = "0.1"
+    "#;
+    let meta = toml::toml! {
+    [file]
+    formatVersion = 1
+
+    [modloader]
+    type = "forge"
+    version = 1614
+    minecraftVersion = "1.7.10"
+
+    [pack]
+    name = "pack name"
+    author = "Your name"
+    description = "A short description, only on one line"
+    version = "0.1"
+    };
+    assert_eq!(
+        read_metadata(&meta_toml).mod_loader.minecraft_version,
+        meta["modLoader"]["minecraftVersion"].as_str().unwrap()
+    );
 }
